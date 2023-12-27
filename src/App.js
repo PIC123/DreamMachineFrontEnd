e47d0c99cb3e4477faebe50b11f4628cda14f7af
Phil Cherner <pcherner@mit.edu>
2023-12-27 00:44:40 -0500

    update background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import background from './background.png'; 
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
@@ -29,26 +30,37 @@ function App() {
     justifyContent: 'center',
     alignItems: 'center',
     height: '100vh',
-    background: 'repeating-radial-gradient(#59d3e0, transparent 100px)',
+    // background: 'repeating-radial-gradient(#59d3e0, transparent 100px)',
   };
 
   return (
-    <div style={style}>
-      <body>
-        <h1>Welcome to Dream Machine</h1>
-          {imageUrl ? (
-            <img
-              src={imageUrl}
-              alt="latest dream"
-              style={{ maxWidth: '100%', maxHeight: '100%' }}
-              className="dream-image"
-            />
-          ) : (
-            <p>Loading image...</p>
-          )}
-        <p className="image-label">Latest Dream</p>
-        <h2>Call +1(479)-348-1521 to submit your dream</h2>
-      </body>
+    <div 
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: 'cover',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}>
+      <div style={style}>
+        <body>
+          <h1>Welcome to Dream Machine</h1>
+            {imageUrl ? (
+              <img
+                src={imageUrl}
+                alt="latest dream"
+                style={{ maxWidth: '100%', maxHeight: '100%' }}
+                className="dream-image"
+              />
+            ) : (
+              <p>Loading image...</p>
+            )}
+          <p className="image-label">Latest Dream</p>
+          <h2>Call +1(479)-348-1521 to submit your dream</h2>
+        </body>
+      </div>
     </div>
   );
 }
